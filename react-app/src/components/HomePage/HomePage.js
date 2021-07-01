751866e6c0e9fbac2b9f4ca11b3b7ad914ffd8cb
kbetker <kevin@betker.org>
2021-07-01 15:24:23 -0500

    hompage is looking good. breaking from this to fix heroku issue

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./HomePage.css"
 import background from "./background_animation-greyscale-Black_Background.gif"
 import logoGif from "./background_animation.gif"
@@ -6,10 +7,21 @@ import logoGif from "./background_animation.gif"
 function HomePage(){
     return(
         <>wat?
-        {/* <div className="homepage_wrapper">
-            <div className="animated_background" style={{backgroundImage: `url(${background})`}}></div>
-            <img src={logoGif} className="logo_gif"></img>
-        </div> */}
+        <div className="homepage_wrapper">
+            <div >
+                <img src={background} className="animated_background"></img>
+                <div className="navBar">
+                    <div className="logo">Logo</div>
+                    <div className="links">
+                        <Link to="/">Log in</Link>
+                        <div className="spacer">|</div>
+                        <Link to="/">Sign Up</Link>
+                        <div className="spacer">|</div>
+                        <Link to="/">Demo</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
        </>
     )
 }
