69a074e223350b373809e66bb46e369562a8fa0a
kbetker <kevin@betker.org>
2021-07-01 14:39:22 -0500

    creating new branch to work on

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -29,9 +29,11 @@ function App() {
 
   return (
     <BrowserRouter>
-    <HomePage />
-      <NavBar />
+      {/* <NavBar /> */}
       <Switch>
+        <Route path="/" exact={true}>
+          <HomePage />
+        </Route>
         <Route path="/login" exact={true}>
           <LoginForm />
         </Route>
@@ -41,15 +43,13 @@ function App() {
         <Route path="/sound">
             <Sound></Sound>
         </Route>
-        {/* thisone */}
+
+
         <ProtectedRoute path="/users" exact={true}>
           <UsersList/>
         </ProtectedRoute>
         <ProtectedRoute path="/users/:userId" exact={true}>
           <User />
-        </ProtectedRoute>
-        <ProtectedRoute path="/" exact={true} >
-          <h1>My Home Page</h1>
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
@@ -57,3 +57,7 @@ function App() {
 }
 
 export default App;
+
+        // <ProtectedRoute path="/" exact={true} >
+        //   <h1>My Home Page</h1>
+        // </ProtectedRoute>
