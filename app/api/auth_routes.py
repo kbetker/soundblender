ac36b6f486a0d63c5e193918c56587869939c7d4
kbetker <kevin@betker.org>
2021-07-08 15:02:17 -0500

    updated the homepage. heroku still not working

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -71,6 +71,7 @@ def sign_up():
         db.session.add(user)
         db.session.commit()
         login_user(user)
+        print(user.to_dict(), "============= wat   =============================")
         return user.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,14 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import "./HomePage.css"
 import background from "./background_animation-greyscale-Black_Background.gif"
 import logoGif from "./logoAnimationGreen.gif"
 import logo from "./homepageLogo.png"
 import LogoutButton from "../auth/LogoutButton"
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import dmBackground from "./dm.jpg"
+import { login } from '../../store/session'
 
 function HomePage(){
     const user = useSelector(state => state.session.user)
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    const onLogin = async (e) => {
+        e.preventDefault();
+        const data = await dispatch(login('demo@aa.io', 'password'));
+        if (data.errors) {
+            alert(data.errors);
+        } else {
+        await history.push(`/users/${data.id}`)
+    }
+    };
+
     return(
         <>
         <div className="homepage_wrapper">
@@ -28,12 +43,13 @@ function HomePage(){
                             <div className="spacer">|</div>
                             <Link to="/sign-up"  className="homeLinks">Sign Up</Link>
                             <div className="spacer">|</div>
-                            <Link to="/"  className="homeLinks">Demo</Link>
+                            <div onClick={onLogin} className="demoHomeLink">Demo</div>
                         </>
 
                     }
                     </div>
                 </div>
+                <img src={dmBackground} className="dmbackground" alt="dungeon master guide" draggable={false}></img>
                 <img src={background} className="animated_background" alt="animated background" draggable={false}></img>
             </div>
         </div>
